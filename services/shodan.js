a2d21e7acb96414b224c9bdcef751925a1c68de3
cornpopcorn <cornpopcorn@duck.com>
2025-03-19 04:01:07 -0700

    Update shodan.js
    Signed-off-by: cornpopcorn <cornpopcorn@duck.com>

--- services/shodan.js
+++ services/shodan.js
@@ -6,15 +6,21 @@ const BASE_URL = 'https://api.shodan.io';
 async function apiRequest(endpoint, params = {}) {
   const urlParams = new URLSearchParams({ key: SHODAN_API_KEY, ...params });
   const url = `${BASE_URL}${endpoint}?${urlParams}`;
-  const response = await safeFetch(url);
-  return response;
+  try {
+    return await safeFetch(url);
+  } catch (error) {
+    if (error.message.includes('status: 404')) {
+      return { error: 'IP not found in Shodan', status: 404 };
+    }
+    throw error;
+  }
 }
 
 export const getHostInformation = (ip, history = false, minify = true) =>
-  apiRequest(`/shodan/host/${ip}`, { history, minify });
+  apiRequest(`/shodan/host/${encodeURIComponent(ip)}`, { history, minify });
 
 export const searchShodan = (query, facets = '', page = 1) =>
-  apiRequest('/shodan/host/search', { query, facets, page: page });
+  apiRequest('/shodan/host/search', { query, facets, page });
 
 export const getHostCount = (query, facets = '') =>
   apiRequest('/shodan/host/count', { query, facets });
